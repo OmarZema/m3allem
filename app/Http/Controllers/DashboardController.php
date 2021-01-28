ef004c67d5770e7b3e7b146289a3f1c29d82d97d
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2021-01-28 15:48:43 +0100

    listing posts

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -13,7 +14,8 @@ class DashboardController extends Controller
 
     public function index()
     {
-        dd(auth()->user()->posts);
+        // dd(Post::find(3)->created_at);
+        // dd(auth()->user()->posts);
         return view('dashboard');
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,4 +12,9 @@ class Post extends Model
     protected $fillable = [
         'content'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
